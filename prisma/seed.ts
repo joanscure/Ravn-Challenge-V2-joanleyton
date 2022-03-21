c2420d38b5a846a2cac664bb9e15157fd33fbb73
Joan <joan.leyton08@gmail.com>
2022-03-21 16:36:30 -0500

    adding more data generation for testing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,43 @@
-import { PrismaClient } from '@prisma/client';
+import faker from '@faker-js/faker';
+import { PrismaClient, SizeProduct } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const category = ['shorts','skirts','pants'];
+  const categories = ['shorts', 'skirts', 'pants'];
+
+  const categoriesId = [];
+  //categories
   for (let i = 0; i < 3; i++) {
-    await prisma.category.create({
+    const category = await prisma.category.create({
       data: {
-        name: category[i],
+        name: categories[i],
       },
     });
+    categoriesId.push(category.id);
   }
+  // products
+  const productsId = [];
+  for (let i = 0; i < 5; i++) {
+    const product = await prisma.product.create({
+      data: {
+        name: faker.unique(faker.commerce.product),
+        size: faker.random.arrayElement([
+          SizeProduct.L,
+          SizeProduct.M,
+          SizeProduct.S,
+          SizeProduct.XL,
+        ]),
+        price: faker.datatype.float({ max: 1000 }),
+        availableStock: faker.datatype.number({ min: 0 }),
+        description: faker.random.words(),
+        categoryId: faker.random.arrayElement(categoriesId),
+      },
+    });
+    productsId.push(product.id);
+  }
+  //users
 
   await prisma.user.create({
     data: {
@@ -23,6 +49,47 @@ async function main() {
       email: 'joan.leyton08@gmail.com',
     },
   });
+  const usersId = [];
+
+  for (let i = 0; i < 12; i++) {
+    const user = await prisma.user.create({
+      data: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        username: faker.unique(faker.internet.userName),
+        password: await bcrypt.hash('any123', 10),
+        role: 'USER',
+        email: faker.unique(faker.internet.email),
+      },
+    });
+    usersId.push(user.id);
+  }
+
+  //orders
+
+  for (let i = 0; i < 12; i++) {
+    const countOrdersOrders = faker.datatype.number({ min: 1, max: 4 });
+    const order = await prisma.order.create({
+      data: {
+        userId: usersId[i],
+        date: faker.date.recent(),
+        totalAmount: faker.datatype.float({ min: 10, max: 900 }),
+      },
+    });
+    for (let i = 0; i < countOrdersOrders; i++) {
+      const price = faker.datatype.float({ min: 2, max: 300 });
+      const quantity = faker.datatype.number({ min: 1, max: 9 });
+      await prisma.orderDetail.create({
+        data: {
+          productId: faker.random.arrayElement(productsId),
+          price: price,
+          quantity: quantity,
+          subtotal: price * quantity,
+          orderId: order.id,
+        },
+      });
+    }
+  }
 }
 
 main()
